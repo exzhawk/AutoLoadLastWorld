c4c71e01529642bf89c5aa3fd26d9af2ae65abbe
Epix <git@exz.me>
2021-03-01 00:45:32 +0800

    bump version to 1.2, sync version in java code

--- src/main/java/me/exz/autoloadlastworld/AutoLoadLastWorld.java
+++ src/main/java/me/exz/autoloadlastworld/AutoLoadLastWorld.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class AutoLoadLastWorld {
     public static final String MODID = "autoloadlastworld";
     public static final String NAME = "Auto load last world";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "@VERSION@";
     static boolean firstTime = true;
 
     @SuppressWarnings("unused")
